301cd2b521078456fdd39acff553e25039d072d3
PearsonJ <pearsonjoshua13131992@gmail.com>
2018-08-30 17:39:31 -0700

    Added basic HTML and CSS build. Need to Add javaScript and Figure out some questionsgit add .

---
+++ assets/javaScript/main.js
@@ -0,0 +1,5 @@
+// first I will want to create the timer and I want it to count down from 2:00
+// I need to make that timer with an interval timmer set to count down every second
+// I will also need to make it so answers are being checked on correctness and the right/wrong % is displayed
+// I will need to research some form commands to figure out how to do that.
+// I want The right/wrong to pop up at the end in a modle. With a gif possibly? 
