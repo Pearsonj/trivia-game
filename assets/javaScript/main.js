0b21ac076433d2f12c85a6f9bcf7fa2af60cf6aa
PearsonJ <pearsonjoshua13131992@gmail.com>
2018-09-05 09:49:16 -0700

    Added some css elements and added a counting down clock for 2 minutes, but would like to maybe change time. Stilll need to add questions and javascript to collect users answers

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -3,3 +3,51 @@
 // I will also need to make it so answers are being checked on correctness and the right/wrong % is displayed
 // I will need to research some form commands to figure out how to do that.
 // I want The right/wrong to pop up at the end in a modle. With a gif possibly? 
+window.onload = function () {
+    $(".btn").on('click', stopwatch.start);
+
+};
+var intervalId;
+var clockRunning = false;
+var stopwatch = {
+    time: 120,
+
+    
+
+    start: function () {
+
+        if (!clockRunning) {
+            intervalId = setInterval(stopwatch.count, 1000);
+            clockRunning = true;
+        }
+    },
+
+    count: function () {
+        stopwatch.time--;
+
+        var converted = stopwatch.timeConverter(stopwatch.time);
+        console.log(converted);
+
+        $('#timer').html('<h1>' + converted + '</h1>');
+    },
+
+    timeConverter: function (t) {
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        if (minutes === 0) {
+            minutes = '00'
+        } 
+        else if (minutes < 10) {
+            munites = '0' + minutes;
+        }
+
+        return minutes + ':' + seconds;
+    }
+
+   
+};
